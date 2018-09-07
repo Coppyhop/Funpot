2e13fb42b3b0ead73503c4d4caf7e473346d17d1
Kyle Bredenkamp <k.j.bredenkamp@outlook.com>
2018-09-06 22:46:03 -0700

    Setup a "Hello World" application as a base and begin lengthy commenting so that others may understand how this works as well.

---
+++ src/main/java/com/kjbre/funpot/Main.java
@@ -0,0 +1,23 @@
+package com.kjbre.funpot;
+/**
+ * Filename: Main.java
+ * Author: Kyle Bredenkamp
+ * Description: This is the main class file for the Funpot project. This is a total rewrite of my
+ *              Ongoing hobby of maintaining a discord bot. Aiming to be fully open-source, this
+ *              will aim to follow style guidelines (for practice) and readability for others to
+ *              learn from my work. I do not aim for this to be the most feature-rich discord bot,
+ *              but I do aim for it to be one of the most customizable.
+ */
+public class Main {
+    /**
+     * This is the main method of the program, I plan on including things such as a debug flag as
+     * one of the arguments, and other useful things. In the future, this will include the JDA
+     * object and the main loop for the bot, but as it stands this is just a simple "hello world"
+     * program.
+     * @param args  The arguments to use when launching the application. Currently none are
+     *              available.
+     */
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+    }
+}
